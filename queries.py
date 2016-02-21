59d3ab4d559f564d1005d5edb00877a051d49b18
CruzanCaramele <letters2hamza@gmail.com>
2016-02-21 11:36:17 +0800

    query for puppies by shelter

--- queries.py
+++ queries.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import asc, desc, Date, create_engine
 from sqlalchemy.orm import sessionmaker
 from puppyshelters import Base, Shelter, Puppy
 
@@ -12,3 +12,21 @@ session = DBSession()
 puppies = session.query(Puppy).order_by(Puppy.name).all()
 for puppy in puppies:
 	print puppy.name
+
+
+# Query all of the puppies that are less than 6 months old organized by the youngest first
+puppies_six_months = session.query(Puppy).filter(Puppy.dateOfBirth >= "2015-06-01").order_by(asc(Puppy.dateOfBirth))
+for puppies in puppies_six_months:
+	print puppies.dateOfBirth
+
+
+# Query all puppies by ascending weight
+puppyWeights = session.query(Puppy).order_by(asc(Puppy.weight)).all()
+for puppyWeight in puppyWeights:
+	print puppyWeight.weight
+
+
+#Query all puppies grouped by the shelter in which they are staying
+puppyHomes = session.query(Puppy).order_by(Puppy.shelter_id)
+for puppyHome in puppyHomes:
+	print puppyHome.name + ' : ' + puppyHome.shelter.name
